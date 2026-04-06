51ac470daf5cf55d5ab78d8df9dc20422d42f538
SalemF915 <safraire@willamette.edu>
2026-04-06 14:55:24 -0700

    compare

--- jscode.js
+++ jscode.js
@@ -49,3 +49,49 @@ async function insert(){
 
     server_test();
 }
+
+
+//compare the similarity of two music files, return a score between 0 and 1, 1 means they are identical
+async function compare(){
+
+    const flattenJSON = (obj, prefix = '') => {
+    return Object.keys(obj).reduce((acc, k) => {
+        const pre = prefix.length ? prefix + '.' : '';
+        if (typeof obj[k] === 'object' && obj[k] !== null && !Array.isArray(obj[k])) {
+        Object.assign(acc, flattenJSON(obj[k], pre + k));
+        } else {
+        acc[pre + k] = obj[k];
+        }
+        return acc;
+    }, {});
+    };
+
+    const getSimilarity = (obj1, obj2) => {
+    const flat1 = flattenJSON(obj1);
+    const flat2 = flattenJSON(obj2);
+
+    const set1 = new Set(Object.entries(flat1).map(e => JSON.stringify(e)));
+    const set2 = new Set(Object.entries(flat2).map(e => JSON.stringify(e)));
+
+    const intersection = new Set([...set1].filter(x => set2.has(x)));
+    const union = new Set([...set1, ...set2]);
+
+    return intersection.size / union.size;
+    };
+
+    const _fileA = document.getElementById('upload1').files[0];
+    const _fileB = document.getElementById('upload2').files[0];
+
+
+    const dataA = JSON.parse(await _fileA.text());
+    const dataB = JSON.parse(await _fileB.text());
+
+    const files = [dataA, dataB];
+
+    const matrix = files.map(file1 => 
+    files.map(file2 => getSimilarity(file1, file2).toFixed(2))
+    );
+
+    console.table(matrix);
+
+}
